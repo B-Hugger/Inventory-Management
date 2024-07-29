d256b93f4aca3863a518b8c26e0bebb3c67e0b91
S-Jones <shamil1.jones@famu.edu>
2024-07-29 18:15:31 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -60,17 +60,15 @@ filter_frame.pack(side = 'top',fill='both')
 
 # Master setting
     ##Label for the dropbox for finding keywords
-find_label = tk.Label(filter_frame, text="Find:", font=("Helvetica", 14))
-find_label.pack(anchor=tk.W)  # Use 'tk.W' instead of 'tkinter.W'
+filter_label_Manufacture = tk.Label(filter_frame, text="Manufacture", font=("Helvetica", 12))
+filter_label_Manufacture.pack(anchor=tk.W)  # Use 'tk.W' instead of 'tkinter.W'
+filter_label_WidthSize = tk.Label(filter_frame, text="Width Size (mm)", font=("Helvetica", 12))
+filter_label_WidthSize.pack(anchor=tk.W)  # Use 'tk.W' instead of 'tkinter.W'
+filter_label_OpCom = tk.Label(filter_frame, text="", font=("Helvetica", 12)) #Operation Comparsion
+filter_label_OpCom.pack(anchor=tk.W)  # Use 'tk.W' instead of 'tkinter.W'
+filter_label_IDNum = tk.Label(filter_frame, text="ID #", font=("Helvetica", 12))
+filter_label_IDNum.pack(anchor=tk.W)  # Use 'tk.W' instead of 'tkinter.W'
 
-find_label_Manufacture = tk.Label(filter_frame, text="Manufacture", font=("Helvetica", 12))
-find_label_Manufacture.pack(anchor=tk.W)  # Use 'tk.W' instead of 'tkinter.W'
-find_label_WidthSize = tk.Label(filter_frame, text="WidthSize", font=("Helvetica", 12))
-find_label_WidthSize.pack(anchor=tk.W)  # Use 'tk.W' instead of 'tkinter.W'
-find_label_OpCom = tk.Label(filter_frame, text="", font=("Helvetica", 12)) #Operation Comparsion
-find_label_OpCom.pack(anchor=tk.W)  # Use 'tk.W' instead of 'tkinter.W'
-find_label_IDNum = tk.Label(filter_frame, text="ID Number", font=("Helvetica", 12))
-find_label_IDNum.pack(anchor=tk.W)  # Use 'tk.W' instead of 'tkinter.W'
 #TreeView
 dataframe = pd.read_csv("inventory.csv")
 table_frame = tk.Frame(master = window,width = 300, height = 500)
@@ -86,22 +84,21 @@ table.pack(anchor = tk.W, fill = "both")
 
     #Dropdown Widgets
 choices_Manufacture = ['', 'Shanghai','Fujikura ','SuperPower']
-branch1_dropdown = ttk.Combobox(filter_frame, values=choices_Manufacture)
+branch1_dropdown = ttk.Combobox(filter_frame, values=choices_Manufacture, width = 10)
 branch1_dropdown.pack(anchor=tk.W,padx=10)
 
-choices_OpCom = ['', '<','>','=','</=','>/=']
-branch2_dropdown = ttk.Combobox(filter_frame, values=choices_OpCom,width=4)
+choices_OpCom = ['', '<','>','=','\u2265','\u2264']
+branch2_dropdown = ttk.Combobox(filter_frame, values=choices_OpCom,width=1)
 branch2_dropdown.pack()
 
 
-branch3_dropdown = ttk.Entry(filter_frame)
+branch3_dropdown = ttk.Entry(filter_frame,width=6)
 branch3_dropdown.pack(anchor=tk.NW, padx=10)
 
-
-
-
-branch4_dropdown = ttk.Entry(filter_frame)
+branch4_dropdown = ttk.Entry(filter_frame,width = 10)
 branch4_dropdown.pack(anchor=tk.N,padx=10)
+
+
 #filter selection
 def getValue(filter):
     return filter.get()
@@ -110,38 +107,36 @@ def getValue(filter):
     #Button Widget
 filter_button = ttk.Button(filter_frame, text = 'Filter', command = lambda: filter(dataframe,table,dataframe.columns,[branch1_dropdown.get(),branch3_dropdown.get(),branch4_dropdown.get()]))
 filter_button.pack()
+
 # Grid
     #Colums
 filter_frame.columnconfigure(0, weight = 1)
 filter_frame.columnconfigure(1, weight = 2)
 filter_frame.columnconfigure(2, weight = 1)
 filter_frame.columnconfigure(3, weight = 1)
-filter_frame.columnconfigure(4, weight = 3)
-filter_frame.columnconfigure(5, weight =2)
+filter_frame.columnconfigure(4, weight = 1, minsize=10)
+
     #Rows
 filter_frame.rowconfigure(0, weight = 1)
 filter_frame.rowconfigure(1, weight = 1)
-filter_frame.rowconfigure(2, weight = 1)
 
 #Placement for the widgets in the grid
 ##Labels
-    ### The 'Find:' label
-find_label.grid(row = 0, column = 0)
 
     ### Labels for the dropdown widgets placements
-find_label_Manufacture.grid(row = 1, column = 1)
-find_label_OpCom.grid(row = 1, column = 2)
-find_label_WidthSize.grid(row = 1, column = 3)
-find_label_IDNum.grid(row = 1, column = 4)
+filter_label_Manufacture.grid(row = 0, column = 0)
+filter_label_OpCom.grid(row = 0, column = 1)
+filter_label_WidthSize.grid(row = 0, column = 2)
+filter_label_IDNum.grid(row = 0, column = 4)
 
     ### Dropdown widgets placements
-branch1_dropdown.grid(row = 2, column = 1)
-branch2_dropdown.grid(row = 2, column = 2, sticky = 'e')
-branch3_dropdown.grid(row = 2, column = 3)
-branch4_dropdown.grid(row = 2, column = 4)
+branch1_dropdown.grid(row = 0, column = 1)
+branch2_dropdown.grid(row = 0, column = 2, sticky = 'e')
+branch3_dropdown.grid(row = 0, column = 3, sticky = 'e')
+branch4_dropdown.grid(row = 0, column = 4, sticky = 'e')
 
     ### Filter button
-filter_button.grid(row = 2, column = 5, padx = 5)
+filter_button.grid(row = 0, column = 5)
 
 
 
