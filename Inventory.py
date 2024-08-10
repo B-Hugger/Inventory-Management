09622a40f9308fc1caaa05934fa6e14151d7ae71
CrypticInventions <102758882+CrypticInventions@users.noreply.github.com>
2024-08-10 02:14:02 -0400

    added additional filter functionality

--- Inventory.py
+++ Inventory.py
@@ -35,13 +35,14 @@ class FilterFrame(tk.Frame):
     def __init__(self, parent, dataframe):
         super().__init__(parent)
         # Frame
-        self.configure(width = 300, height = 400, borderwidth = 10)
+        self.configure(width=300, height=400, borderwidth=10)
         self.dataframe = pd.read_csv(dataframe)
         self.pack_propagate(False)
         self.pack(side='top', fill='both')
         self.createDropdown()
         self.create_table()
         #Dropdown Widgets
+
     def createDropdown(self):
         manufacture_frame = tk.Frame(master=self)
         width_frame = tk.Frame(master=self)
@@ -68,50 +69,60 @@ class FilterFrame(tk.Frame):
         filter_label_IDNum = tk.Label(id_frame, text="ID #", font=("Helvetica", 12))
         filter_label_IDNum.pack(side="left", anchor=tk.W)  # Use 'tk.W' instead of 'tkinter.W'
         choices_manufacture = ['', 'Shanghai', 'Fujikura', 'SuperPower']
-        self.branch1_dropdown = DropDown(parent = manufacture_frame, column = "Manufacturer", selections =choices_manufacture, width = 11)
+        self.branch1_dropdown = DropDown(parent=manufacture_frame, column="Manufacturer",
+                                         selections=choices_manufacture, width=11)
 
         self.branch1_dropdown.pack(anchor=tk.W, padx=0)
         choices_OpCom = ['', '<', '>', '=', '\u2265', '\u2264']
-        self.branch2_dropdown = DropDown(parent = width_frame, column = None, selections = choices_OpCom, width = 1)
+        self.branch2_dropdown = DropDown(parent=width_frame, column=None, selections=choices_OpCom, width=1)
         self.branch2_dropdown.pack(side="left", anchor=tk.W)
 
-        self.branch3_dropdown = Entry(parent = width_frame, column = "Size", width = 3)
+        self.branch3_dropdown = Entry(parent=width_frame, column="Size", width=3)
         self.branch3_dropdown.pack(side="left", anchor=tk.E)
 
-        self.branch4_dropdown = Entry(parent = id_frame, column = "Identification", width = 10)
+        self.branch4_dropdown = Entry(parent=id_frame, column="Identification", width=10)
         self.branch4_dropdown.pack(side="left", anchor=tk.W)
 
         # Button Widget
         filter_button = ttk.Button(self, text='Filter', command=lambda: self.filter())
         filter_button.grid(row=0, column=3)
 
-
-
     def refresh(self, table):
         for row in table.get_children():
             table.delete(row)
+
     def create_table(self):
-        self.table = ttk.Treeview(master = self)
+        table_frame = tk.Frame(master = self)
+        table_frame.grid(row = 1, column = 0, pady=20, ipady=80,columnspan=5, sticky="swse" )
+        self.table = ttk.Treeview(master= table_frame)
         self.table.configure(columns=tuple(self.dataframe.columns), show="headings")
+        self.table.pack(fill=tk.BOTH, expand=True)
         for column in tuple(self.dataframe.columns):
             self.table.heading(column, text=column)
             self.table.column(column, anchor=tk.CENTER)
         for rows in self.dataframe.itertuples(index=False, name=None):
             self.table.insert(parent='', index=tk.END, values=tuple(rows))
-        self.table.grid(row = 1, column = 0,pady = 20,ipady = 80, columnspan = 5, sticky = "swse",)
+        scrollbar = ttk.Scrollbar(self.table, orient=tk.VERTICAL, command = self.table.yview)
+        self.table.configure(yscrollcommand=scrollbar.set)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.text = tk.StringVar()
+        self.text.set("Total Items: " + str(len(self.table.get_children())))
+        item_amount = tk.Label(master= table_frame, textvariable=self.text)
+        item_amount.pack(side="left")
 
     def filter(self):
         newdataframe = self.dataframe
         columns = self.returnfilters()
-        dictionary = {'<': '<', '>' : '>', '=' : "==", '\u2265' : '>=', '\u2264' : '<='}
+        dictionary = {'<': '<', '>': '>', '=': "==", '\u2265': '>=', '\u2264': '<='}
         flag = False
-        print(columns[0].get(), columns[1].get(),columns[2].get(),columns[3].get())
+        print(columns[0].get(), columns[1].get(), columns[2].get(), columns[3].get())
         for index, entry in enumerate(columns):
             if entry.get() != '':
                 try:
-                    if entry.get() in  dictionary.keys() and columns[2].get() != '':
+                    if entry.get() in dictionary.keys() and columns[2].get() != '':
                         print(dictionary[entry.get()])
-                        newdataframe = newdataframe.query(f"{columns[2].column} {dictionary[entry.get()]} {columns[2].get()}")
+                        newdataframe = newdataframe.query(
+                            f"{columns[2].column} {dictionary[entry.get()]} {columns[2].get()}")
                         flag = True
                     elif flag is True:
                         flag = False
@@ -124,17 +135,24 @@ class FilterFrame(tk.Frame):
         self.refresh(self.table)
         for rows in newdataframe.itertuples(index=False):
             self.table.insert(parent='', index=tk.END, values=tuple(rows))
+        self.text.set("Total Items: " + str(len(self.table.get_children())))
+
+
+
     def returnfilters(self):
-        return [self.branch1_dropdown,self.branch2_dropdown, self.branch3_dropdown, self.branch4_dropdown]
+        return [self.branch1_dropdown, self.branch2_dropdown, self.branch3_dropdown, self.branch4_dropdown]
+
 
 class DropDown(ttk.Combobox):
-    def __init__(self,parent,column,width,selections):
-        super().__init__(master = parent,width = width,values = selections)
+    def __init__(self, parent, column, width, selections):
+        super().__init__(master=parent, width=width, values=selections)
         self.selections = selections
         self.column = column
+
+
 class Entry(tk.Entry):
-    def __init__(self,parent,column, width):
-        super().__init__(master = parent, width = width)
+    def __init__(self, parent, column, width):
+        super().__init__(master=parent, width=width)
         self.column = column
 
 
