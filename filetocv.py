fd27b6c2d24abb5dcb297be5370b120ee2779904
S-Jones <shamil1.jones@famu.edu>
2024-08-12 17:00:49 -0400

    Update filetocv.py

--- filetocv.py
+++ filetocv.py
@@ -2,22 +2,34 @@ import os
 import csv
 #file to 2d array:
 folderPath = r'/workspaces/Inventory-Management/Invertory'
+
 def getdir(dir):
     fileNames = os.listdir(dir)
     fileNameList = []
 
     for fileName in fileNames:
+
         # splits filename and extension then retreives filename without extension
         fileName = fileName.split(".")[0]
         # remove all extensions
 
+
         # Split the file name by '_' and append the components to the inner list
         components = fileName.split('_')
+
+        # Deletes the 'mm' from the second element in the array
+        components[1] = components[1].replace('mm', '')
+
+
         fileNameList.append(components)
 
     return fileNameList
 
+# Example usage
+fileNameList = getdir(folderPath)
 
+# Print the array
+print(fileNameList)
 
 
 
